7e6c0b968a502dd3aed16920a3c6556a25442fc5
masahiro96848 <feetwcpic9436@gmail.com>
2022-09-20 11:32:34 +0900

    feat: タスクを編集する機能を作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect} from "react";
-import { AddTodo } from "./components/AddTodo";
 import './index.css'
 
 export const App = ()  => {
     const [todo, setTodo] = useState('');
     const [todos, setTodos] = useState([])
-    const [editTodo, setEditTodo] = useState(false)
+    const [isEditing, setIsEditing] = useState(false)
     const [currentTodo, setCurrentTodo] = useState({})
 
     const handleAddTodo = (event) => {
@@ -27,30 +26,73 @@ export const App = ()  => {
 
         setTodos(removeItem)
     }
+    const handleEditInputChange = (event) => {
+        setCurrentTodo({ ...currentTodo, text: event.target.value })
+        console.log(currentTodo)
+    }
+    const handleEditClick = (todo) => {
+        setIsEditing(true)
+        setCurrentTodo({ ...todo })
+    }
+    const handleUpdateTodo = (id, updateTodo) => {
+        const updateItem = todos.map((todo) => {
+            return todo.id === id ? updateTodo : todo;
+        })
+        setIsEditing(false)
+        setTodos(updateItem)
+    }
+    const handleEditFormSubmit = (event) => {
+        event.preventDefault()
+
+        handleUpdateTodo(currentTodo.id, currentTodo)
+    }
     
     return (
         <>
             <div className="container">
                 <h1 className="title">Todo List</h1>
-                <AddTodo
-                    todo={todo}
-                    handleAddTodo={handleAddTodo}
-                    handleSubmit={handleSubmit}
-                />
-            <section className="container-area">
-                <div>
-                    <ul className="list-row">
-                        {todos.map((todo) => {
-                            return (
-                            <li key={todo.id} className="list" >{todo.text}
-                            <i className="fas fa-pen-square fa-lg"></i>
-                            <i onClick={() => handleDelete(todo.id)} className="fa fa-trash fa-lg" aria-hidden="true"></i>
-                            </li>
-                            )
-                        })}
-                    </ul>
-                </div>
-            </section>
+                <section className="container-area">
+                    {isEditing ? (
+                        <form onSubmit={handleEditFormSubmit}>
+                            <h2>Edit Todo</h2>
+                            <input 
+                                type="text" 
+                                name="editTodo"
+                                placeholder="Edit todo"
+                                value={currentTodo.text}
+                                onChange={handleEditInputChange}
+                            />
+                            <button type="submit">更新</button>
+                            <button onClick={() => setIsEditing(false)}>キャンセル</button>
+                        </form>
+                    ): (
+                    <div>
+                        <form action="" onSubmit={handleSubmit}>
+                            <input
+                                className="input-area" 
+                                placeholder="Todoを追加" 
+                                value={todo}
+                                onChange={handleAddTodo}
+                                type="text"
+                            />
+                        </form>
+                    </div>
+                    )}
+                </section>
+                <section className="container-area">
+                    <div>
+                        <ul className="list-row">
+                            {todos.map((todo) => {
+                                return (
+                                <li key={todo.id} className="list" >{todo.text}
+                                <i onClick={() => handleEditClick(todo)} className="fas fa-pen-square fa-lg"></i>
+                                <i onClick={() => handleDelete(todo.id)} className="fa fa-trash fa-lg" aria-hidden="true"></i>
+                                </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                </section>
             </div>
         </>
     )
--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -2,19 +2,5 @@ import React from "react";
 
 export const AddTodo = (props) => {
     const { todo, handleSubmit, handleAddTodo} = props
-    return (
-        <section className="container-area">
-            <div>
-                <form action="" onSubmit={handleSubmit}>
-                    <input
-                        className="input-area" 
-                        placeholder="Todoを追加" 
-                        value={todo}
-                        onChange={handleAddTodo}
-                        type="text"
-                    />
-                </form>
-            </div>
-        </section>
-    )
+    
 }
